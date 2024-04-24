1e89bf39443876253f6c57db5ff75adf78a5152c
EduardoAquinta <edwoodashton@yahoo.co.uk>
2024-04-24 15:58:45 +0100

    got back to simple list, move code to its own file, refactor listMessages function

--- auth.js
+++ auth.js
@@ -70,27 +70,33 @@ async function authorize() {
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
+let pageToken;
 
-async function listMessages(auth) {
-    const gmail = google.gmail({version: 'v1', auth});
-    try {
-        const res = await gmail.users.messages.list({
-            userId: 'me',
-            q: 'is:unread', // Filter for unread messages
-        });
-        const messages = res.data.messages;
-        if (messages.length) {
-            console.log(`There are ${messages.length} unread messages`)
-            console.log('Unread Messages:');
-            messages.forEach((message) => {
-                console.log('- %s', message.id);
-            });
-        } else {
-            console.log('No unread messages found.');
-        }
-    } catch (error) {
-        console.error('The API returned an error:', error);
-    }
-}
 
-authorize().then(listMessages).catch(console.error);
+// async function listMessages(auth) {
+//     const gmail = google.gmail({version: 'v1', auth});
+//     try {
+//         const res =  await gmail.users.messages.list({
+//             userId: 'me',
+//             q: 'is:unread', // Filter for unread messages
+//             maxResults: 10,
+//         });
+//         const messages = res.data.messages;
+//         let nextPageToken = res.data.nextPageToken;
+//         if (messages.length) {
+//             console.log(`There are ${messages.length} unread messages`)
+//             console.log("Next page token - ", nextPageToken)
+//             console.log('Unread Messages:');
+//             messages.forEach((message) => {
+//                 console.log('- %s', message.id);
+//             });
+//         } else {
+//             console.log('No unread messages found.');
+//         }
+//     } catch (error) {
+//         console.error('The API returned an error:', error);
+//     }
+// }
+
+
+module.exports = authorize;
